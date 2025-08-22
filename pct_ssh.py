8f3cdf4ea717de14d73553c72fedce7544e478fe
Gerhard Burger <burger.ga@gmail.com>
2025-08-22 22:01:37 +0200

    Replace pipes.quote with shlex.quote (#7)
    Fixes #6

--- pct_ssh.py
+++ pct_ssh.py
@@ -28,7 +28,6 @@ import errno
 import fcntl
 import hashlib
 import os
-import pipes
 import pty
 import shlex
 import subprocess
@@ -1356,7 +1355,7 @@ class Connection(ConnectionBase):
 
         ssh_executable = self.get_option("ssh_executable")
         h = self.container_name
-        pct_cmd = "sudo pct exec %s -- %s" % (pipes.quote(h), cmd)
+        pct_cmd = "sudo pct exec %s -- %s" % (shlex.quote(h), cmd)
         if in_data:
             cmd = self._build_command(ssh_executable, "ssh", self.host, pct_cmd)
         else:
@@ -1381,14 +1380,14 @@ class Connection(ConnectionBase):
                 if len(in_data) == 0:
                     # define a shortcut for empty files - nothing ro read so
                     # the ssh pipe will hang
-                    cmd = "touch %s; echo -n done" % pipes.quote(out_path)
+                    cmd = "touch %s; echo -n done" % shlex.quote(out_path)
                 else:
                     # regular command
-                    cmd = "cat > %s; echo -n done" % pipes.quote(out_path)
+                    cmd = "cat > %s; echo -n done" % shlex.quote(out_path)
                 h = self.container_name
                 pct_cmd = "sudo pct exec %s -- /bin/sh -c %s" % (
-                    pipes.quote(h),
-                    pipes.quote(cmd),
+                    shlex.quote(h),
+                    shlex.quote(cmd),
                 )
                 if in_data:
                     cmd = self._build_command(ssh_executable, "ssh", self.host, pct_cmd)
@@ -1404,14 +1403,14 @@ class Connection(ConnectionBase):
                 if len(in_data) == 0:
                     # define a shortcut for empty files - nothing ro read so
                     # the ssh pipe will hang
-                    cmd = "touch %s; echo -n done" % pipes.quote(out_path)
+                    cmd = "touch %s; echo -n done" % shlex.quote(out_path)
                 else:
                     # regular command
-                    cmd = "cat > %s; echo -n done" % pipes.quote(out_path)
+                    cmd = "cat > %s; echo -n done" % shlex.quote(out_path)
                 h = self.container_name
                 pct_cmd = "sudo pct exec %s -- %s" % (
-                    pipes.quote(h),
-                    pipes.quote(cmd),
+                    shlex.quote(h),
+                    shlex.quote(cmd),
                 )
                 if in_data:
                     cmd = self._build_command(ssh_executable, "ssh", self.host, pct_cmd)
@@ -1428,11 +1427,11 @@ class Connection(ConnectionBase):
         display.vvv("FETCH {0} TO {1}".format(in_path, out_path), host=self.host)
         ssh_executable = self.get_option("ssh_executable")
 
-        cmd = "cat < %s" % pipes.quote(in_path)
+        cmd = "cat < %s" % shlex.quote(in_path)
         h = self.container_name
         pct_cmd = "sudo pct exec %s -- %s" % (
-            pipes.quote(h),
-            pipes.quote(cmd),
+            shlex.quote(h),
+            shlex.quote(cmd),
         )
 
         cmd = self._build_command(ssh_executable, "ssh", self.host, pct_cmd)
